f471688664e9dddcf52b4e11b9d436210401f7af
Niki Kiedanski <nicole@192.168.1.5>
2022-10-05 20:40:01 -0300

    se agrega prueba con sequencia larga

--- exact_matches.py
+++ exact_matches.py
@@ -15,19 +15,3 @@ def exact_matches(motive, sequence):
 
 
 
-
-# def exact_matches(motive, sequence):
-#     occurrences = 0
-#     positions = []
-#     for index in range(0, len(sequence)-len(motive)+1):
-#         nucleotide_match = 0
-#         for nucleotide in motive:
-#             if nucleotide == sequence[index]:
-#                 index += 1
-#                 nucleotide_match += 1
-#             else:
-#                 break
-#         if nucleotide_match == len(motive):
-#             occurrences += 1
-#             positions.append(index)
-#     return occurrences, positions
--- inexact_matches.py
+++ inexact_matches.py
@@ -13,20 +13,3 @@ def inexact_matches(motive, sequence, mismatches_limit):
             positions.append(index_seq)
     return occurrences, positions
 
-
-# def inexact_matches(motive, sequence, mismatches_limit):
-#     occurrences = 0
-#     positions = []
-#     for index in range(0, len(sequence)-len(motive)+1):
-#         mismatches_count = 0
-#         for nucleotide in motive:
-#             if nucleotide == sequence[index]:
-#                 index += 1
-#             else:
-#                 mismatches_count += 1
-#                 if mismatches_count > mismatches_limit:
-#                     break
-#         if mismatches_count <= mismatches_limit:
-#             occurrences += 1
-#             positions.append(index-len(motive))
-#     return occurrences, positions
